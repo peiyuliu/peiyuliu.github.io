8410364cc90a86f9aa531f1f64105af1272eb022
peiyuliu <betty05010@gmail.com>
2015-10-25 15:53:08 +0200

    初始化設定

---
+++ src/app.js
@@ -0,0 +1,11 @@
+//CSS
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../src/css/index.css"
+
+import loremIpsum from 'lorem-ipsum'
+import randomColor from "randomcolor"
+
+import $ from 'jquery'; 
+
+console.log('hello world!')
+console.log($('body'))

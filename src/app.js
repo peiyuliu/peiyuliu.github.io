20916ea05871ac50a3de3bfbcb12673a04b13bdd
Hung-Han Chen <chenhungh@gmail.com>
2015-10-28 22:45:47 +0200

    i made some examples, such as variables, arrays, function and objects

--- src/app.js
+++ src/app.js
@@ -2,10 +2,50 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../src/css/index.css"
 
-import loremIpsum from 'lorem-ipsum'
-import randomColor from "randomcolor"
+//var p = document.querySelector(".pega")
+//
+//var status = 0
+//
+//function show() {
+//    if (status === 0) {
+//        console.log('into status === 0')
+//        document.querySelector(".pegaProject").style.display = 'block'
+//        status = 1
+//    } else if (status === 1) {
+//        console.log('into status === 1')
+//        document.querySelector(".pegaProject").style.display = 'none'
+//        status = 0
+//    }
+//}
+//
+//p.addEventListener("click", show)
 
-import $ from 'jquery'; 
+//console.log(1)
+//console.log(2)
+//console.log(3)
+//console.log(4)
+//console.log(5)
 
-console.log('hello world!')
-console.log($('body'))
+//i++ === i=i+1
+
+//for (var i = 1; i < 6; i=i+1) {
+//    console.log(i)
+//}
+
+var a = 1
+var b = 'sdfsdfsdf' //string
+var apples = [5,9,22,22,51,61,12,123]
+var object = {
+    a: {
+        aa: [5,9,22,22,51,61,12,123],
+        bb: 1
+    },
+    b: {
+        bb: 'sss',
+        dd: true
+    }
+}
+
+console.log(apples[0])
+console.log(object.a.bb[5])
+console.log(oject)

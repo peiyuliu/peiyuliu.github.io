638e981aed9e8238dd2e0d1cbd92a2715e9539b8
peiyuliu <betty05010@gmail.com>
2015-10-28 22:49:26 +0200

    add class=pega

--- src/app.js
+++ src/app.js
@@ -2,10 +2,4 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../src/css/index.css"
 
-import loremIpsum from 'lorem-ipsum'
-import randomColor from "randomcolor"
-
-import $ from 'jquery'; 
-
 console.log('hello world!')
-console.log($('body'))
